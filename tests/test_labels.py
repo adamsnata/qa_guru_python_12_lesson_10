0af01e7a148a9c125494c93bc050d7903c84ddd9
natal <svetnata74@mail.ru>
2024-04-19 13:18:47 +0200

    ckk

--- tests/test_labels.py
+++ tests/test_labels.py
@@ -2,7 +2,7 @@ import allure
 from allure_commons.types import Severity
 
 
-def test_dynamic_labels(browser_management):
+def test_dynamic_labels():
     allure.dynamic.tag("web")
     allure.dynamic.severity(Severity.BLOCKER)
     allure.dynamic.feature("Задачи в репозитории")
@@ -10,12 +10,23 @@ def test_dynamic_labels(browser_management):
     allure.dynamic.link("https://github.com", name="Testing")
     pass
 
+
+
 @allure.tag("web")
 @allure.severity(Severity.CRITICAL)
 @allure.label("owner", "eroshenkoam")
 @allure.feature("Задачи в репозитории")
 @allure.story("Авторизованный пользователь может создать задачу в репозитории")
 @allure.link("https://github.com", name="Testing")
+def test_decorator_labels():
+    pass
 
-def test_decorator_labels(browser_management):
+
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "eroshenkoam")
+@allure.feature("Задачи в репозитории")
+@allure.story("Авторизованный пользователь может создать задачу в репозитории")
+@allure.link("https://github.com", name="Testing")
+def test_decorator_labels_1():
     pass
